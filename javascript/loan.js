d73e620e8f04aaf4e1776b782d562c689bbf7dc7
Jasmin Sultana Shimu <jasmin851t626@gmail.com>
2026-01-13 01:19:28 +0600

    rate borrower and make offer page connected

--- javascript/loan.js
+++ javascript/loan.js
@@ -7,15 +7,4 @@
             });
         });
 
-        // Button interactions
-        document.querySelectorAll('.btn-offer').forEach(btn => {
-            btn.addEventListener('click', () => alert('Make an offer'));
-        });
-
-        document.querySelectorAll('.btn-view').forEach(btn => {
-            btn.addEventListener('click', () => alert('View image'));
-        });
-
-        document.querySelectorAll('.btn-rating').forEach(btn => {
-            btn.addEventListener('click', () => alert('Submit rating'));
-        });
+   
